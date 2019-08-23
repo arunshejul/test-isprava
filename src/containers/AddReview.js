413a9b70708065a011facfe5927ee9ee80389206
arunshejul <arunshejul@gmail.com>
2019-08-23 16:34:50 +0530

    Complete

--- src/containers/AddReview.js
+++ src/containers/AddReview.js
@@ -5,13 +5,7 @@ import "react-datepicker/dist/react-datepicker.css";
 
 class AddReview extends Component {
   state = {
-    nameOfVilla: "",
-    dateOfVisit: new Date(),
-    pinCode: "",
-    nameOfOwner: "",
-    noteVillaSurrounding: "",
-    noteVillaConstruction: "",
-    noteVillaDecor: ""
+    dateOfVisit: new Date()
   };
 
   handleDateChange = value => {
@@ -20,156 +14,94 @@ class AddReview extends Component {
     });
   };
 
-  handleTextClear = () => {
-    this.setState({
-      nameOfVilla: "",
-      dateOfVisit: "",
-      pinCode: "",
-      nameOfOwner: "",
-      noteVillaSurrounding: "",
-      noteVillaConstruction: "",
-      noteVillaDecor: ""
-    });
-  };
-
-  handleSubmit = () => {
-    const {
-      nameOfVilla,
-      dateOfVisit,
-      pinCode,
-      nameOfOwner,
-      noteVillaSurrounding,
-      noteVillaConstruction,
-      noteVillaDecor
-    } = this.state;
-
+  handleSubmit = event => {
     let str = localStorage.getItem("reviews");
     let reviews = JSON.parse(str);
     if (!reviews) {
       reviews = [];
     }
-
     reviews.push({
       id: Math.floor(Date.now() / 1000),
-      nameOfVilla: nameOfVilla,
-      dateOfVisit: dateOfVisit,
-      pinCode: pinCode,
-      nameOfOwner: nameOfOwner,
-      noteVillaSurrounding: noteVillaSurrounding,
-      noteVillaConstruction: noteVillaConstruction,
-      noteVillaDecor: noteVillaDecor
+      nameOfVilla: event.target[0].value,
+      dateOfVisit: this.state.dateOfVisit,
+      pinCode: event.target[2].value,
+      nameOfOwner: event.target[3].value,
+      noteVillaSurrounding: event.target[4].value,
+      noteVillaConstruction: event.target[5].value,
+      noteVillaDecor: event.target[6].value
     });
 
     localStorage.setItem("reviews", JSON.stringify(reviews));
+    console.log(JSON.stringify(reviews));
+
+    alert("Data saved!");
   };
 
   render() {
     return (
       <Container>
         <br />
-        <Row>
-          <Col xs={6} sm={6} md={6} lg={6}>
-            <Form.Group controlId="formBasic1">
-              <Form.Label>Name of the villa</Form.Label>
-              <Form.Control
-                type="text"
-                value={this.state.nameOfVilla}
-                placeholder="Name of the villa"
-                onChange={txt => this.setState({ nameOfVilla: txt.value })}
-              />
-            </Form.Group>
-          </Col>
-          <Col xs={6} sm={6} md={6} lg={6}>
-            <Form.Group controlId="formBasic2">
-              <Form.Label>Date of visit</Form.Label>
-              <br />
-              <DatePicker selected={this.state.dateOfVisit} onChange={this.handleDateChange} />
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={6} sm={6} md={6} lg={6}>
-            <Form.Group controlId="formBasic3">
-              <Form.Label>PinCode</Form.Label>
-              <Form.Control
-                type="number"
-                maxLength={5}
-                value={this.state.pinCode}
-                placeholder="Pincode"
-                onChange={txt => this.setState({ pinCode: txt.value })}
-              />
-            </Form.Group>
-          </Col>
-          <Col xs={6} sm={6} md={6} lg={6}>
-            <Form.Group controlId="formBasic4">
-              <Form.Label>Owner's name (optional)</Form.Label>
-              <Form.Control
-                type="text"
-                value={this.state.nameOfOwner}
-                placeholder="Owner's name (optional)"
-                onChange={txt => this.setState({ nameOfOwner: txt.value })}
-              />
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={6} sm={6} md={6} lg={6}>
-            <Form.Group controlId="formBasic5">
-              <Form.Label>A note about the surrounding area of the villa</Form.Label>
-              <Form.Control
-                required
-                type="text"
-                value={this.state.noteVillaSurrounding}
-                placeholder="A note about the surrounding area of the villa"
-                onChange={txt => this.setState({ noteVillaSurrounding: txt.value })}
-              />
-            </Form.Group>
-          </Col>
-          <Col xs={6} sm={6} md={6} lg={6}>
-            <Form.Group controlId="formBasic6">
-              <Form.Label>A note about the construction quality of the villa</Form.Label>
-              <Form.Control
-                required
-                type="text"
-                value={this.state.noteVillaConstruction}
-                placeholder="A note about the construction quality of the villa"
-                onChange={txt => this.setState({ noteVillaConstruction: txt.value })}
-              />
-            </Form.Group>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={6} sm={6} md={6} lg={6}>
-            <Form.Group controlId="formBasic7">
-              <Form.Label>A note about the villa decor</Form.Label>
-              <Form.Control
-                required
-                type="text"
-                value={this.state.noteVillaDecor}
-                placeholder="A note about the villa decor"
-                onChange={txt => this.setState({ noteVillaDecor: txt.value })}
-              />
-            </Form.Group>
-          </Col>
-          <Col xs={6} sm={6} md={6} lg={6} />
-        </Row>
-        <Row>
-          <Col
-            xs={3}
-            sm={3}
-            md={3}
-            lg={3}
-            style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between" }}
-          >
-            <Button variant="primary" onClick={this.handleSubmit}>
-              Add Review
-            </Button>
-
-            <Button variant="secondary" onClick={this.handleTextClear}>
-              Clear
-            </Button>
-          </Col>
-        </Row>
+        <form onSubmit={this.handleSubmit}>
+          <Row>
+            <Col xs={6} sm={6} md={6} lg={6}>
+              <Form.Group controlId="formBasic1">
+                <Form.Label>Name of the villa</Form.Label>
+                <Form.Control required type="text" placeholder="Name of the villa" />
+              </Form.Group>
+            </Col>
+            <Col xs={6} sm={6} md={6} lg={6}>
+              <Form.Group controlId="formBasic2">
+                <Form.Label>Date of visit</Form.Label>
+                <br />
+                <DatePicker selected={this.state.dateOfVisit} onChange={this.handleDateChange} />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={6} sm={6} md={6} lg={6}>
+              <Form.Group controlId="formBasic3">
+                <Form.Label>PinCode</Form.Label>
+                <Form.Control required type="number" maxLength={5} placeholder="Pincode" />
+              </Form.Group>
+            </Col>
+            <Col xs={6} sm={6} md={6} lg={6}>
+              <Form.Group controlId="formBasic4">
+                <Form.Label>Owner's name (optional)</Form.Label>
+                <Form.Control type="text" placeholder="Owner's name (optional)" />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={6} sm={6} md={6} lg={6}>
+              <Form.Group controlId="formBasic5">
+                <Form.Label>A note about the surrounding area of the villa</Form.Label>
+                <Form.Control required type="text" placeholder="A note about the surrounding area of the villa" />
+              </Form.Group>
+            </Col>
+            <Col xs={6} sm={6} md={6} lg={6}>
+              <Form.Group controlId="formBasic6">
+                <Form.Label>A note about the construction quality of the villa</Form.Label>
+                <Form.Control required type="text" placeholder="A note about the construction quality of the villa" />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={6} sm={6} md={6} lg={6}>
+              <Form.Group controlId="formBasic7">
+                <Form.Label>A note about the villa decor</Form.Label>
+                <Form.Control required type="text" placeholder="A note about the villa decor" />
+              </Form.Group>
+            </Col>
+            <Col xs={6} sm={6} md={6} lg={6} />
+          </Row>
+          <Row>
+            <Col xs={3} sm={3} md={3} lg={3}>
+              <Button variant="primary" type="submit">
+                Add Review
+              </Button>
+            </Col>
+          </Row>
+        </form>
       </Container>
     );
   }
--- src/containers/ViewReview.js
+++ src/containers/ViewReview.js
@@ -3,7 +3,7 @@ import { Container, ListGroup, Button } from "react-bootstrap";
 
 class ViewReview extends Component {
   state = {
-    reviews: []
+    data: []
   };
 
   componentDidMount() {
@@ -12,28 +12,38 @@ class ViewReview extends Component {
     if (!reviews) {
       reviews = [];
     }
-    this.setState(reviews);
-    console.log("@@@@@@@@@@@@@@@@");
-    console.log(JSON.stringify(reviews));
+    this.setState({
+      data: reviews
+    });
   }
 
-  handleDelete = () => {
-    const reviews = JSON.parse(localStorage.getItem("reviews"));
+  handleDelete = deleteid => {
+    let str = localStorage.getItem("reviews");
+    let reviews = JSON.parse(str);
+    if (reviews) {
+      reviews = reviews.filter(function(obj) {
+        return obj.id !== deleteid;
+      });
+    }
+    this.setState({
+      data: reviews
+    });
+    localStorage.setItem("reviews", JSON.stringify(reviews));
+    alert("Data deleted!");
   };
 
   render() {
     return (
       <Container>
-        <ListGroup variant="flush">
-          {this.state.reviews.map(item => (
+        <ListGroup>
+          {this.state.data.map(item => (
             <ListGroup.Item key={item.id}>
-              {item.nameOfVilla}
-              <br />
-              {item.dateOfVisit}
-              <br />
-              {item.pinCode}
-              <br />
-              <Button deleteid={item.id} variant="danger" onClick={this.handleDelete}>
+              <p>Name of Villa : {item.nameOfVilla}</p>
+              <p>Date of visit : {item.dateOfVisit}</p>
+              <p>A note about the surrounding area of the villa : {item.noteVillaSurrounding}</p>
+              <p>A note about the construction quality of the villa : {item.noteVillaConstruction}</p>
+              <p>A note about the villa decor : {item.noteVillaDecor}</p>
+              <Button variant="danger" onClick={() => this.handleDelete(item.id)}>
                 Delete
               </Button>
             </ListGroup.Item>
